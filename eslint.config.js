6a5a5c71be7bfc8d88afc5ce9c09f62d8c2e2609
Gloria <gloria.palm7810@gmail.com>
2024-02-08 13:20:50 +0100

    Bike rental simulation

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const js = require("@eslint/js");
+const lintRules = require("./eslint.rules.js").lintRules;
+const globals = require("globals");
+
+module.exports = [
+    {
+        ignores: ["**/coverage/", "**/leaflet.js", "admin/", "customer-webb", "phone-app"]
+    },
+
+    js.configs.recommended,
+
+    {
+        files: ["eslint.*.js", "backend/**/*.js", "bike-sim/**/*.js"],
+
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "commonjs",
+            globals: globals.node
+        },
+
+        ...lintRules
+    },
+
+    {
+        files: ["backend/**/*.mjs", "bike-sim/**/*.mjs"],
+
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: globals.browser
+        },
+
+        ...lintRules
+    }
+];
+
